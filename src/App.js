85156c293fea814f10ab4e822d32b2bb89731ff6
d26martian <d26martian@d26martian.local>
2022-11-24 21:34:19 +0300

    lesson 1

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { Form } from './components/classes/Form'
+import { Form as FormFunc } from './components/func/Form'
+
+export function App () {
+  const [toggle, setToggle] = useState(true)
+  const [arr, setArr] = useState([{ name: 'Biba' }, { name: 'Boba' }, { name: 'Giga' }, { name: 'Goga' }])
+  return (
+    <>
+      <Form />
+      <hr />
+      <button onClick={() => setToggle(!toggle)}>{toggle ? 'hide' : 'show'}</button>
+      {toggle && <FormFunc title='Function Component!!!!!!' />}
+      <ul>
+        {arr.map((item) => (
+          <li>{item.name}</li>
+        ))}
+      </ul>
+    </>
+  )
+}
+
+// export default App
